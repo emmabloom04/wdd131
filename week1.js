4c9d26055f33f98acd2960c059920ca59918e449
emmabloom04 <blo23006@byui.edu>
2024-09-18 09:43:49 -0600

    finishing second javascript example, starting prove

---
+++ week1.js
@@ -0,0 +1,7 @@
+const myApples = 4;
+const friendApples = 1;
+let total = myApples + friendApples;
+
+document.getElementById("myAppleElement").textContent = myApples;
+document.getElementById("friendAppleElement").textContent = friendApples;
+document.getElementById("totalApplesElement").textContent = total;
